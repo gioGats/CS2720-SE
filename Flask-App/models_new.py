da42bae4d100ffa014ed62d2eb8f883ac1176eea
gioGats <ryangiarusso@gmail.com>
2016-04-07 16:37:31 -0500

    Fixing syntax errors

--- Flask-App/models_new.py
+++ Flask-App/models_new.py
@@ -19,47 +19,45 @@ class Items(db.Model):
     def __repr__(self):
         return '{} {}'.format(self.type, self.price)
 # TODO Merge Items table and Inventory table
-"""
+
+
 class Inventory(db.Model):
     __tablename__ = "inventory"
 
-    item_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
-    item_type =  = db.Column(db.String)
-    inventory_cost =  = db.Column(db.Float)
-    expiration_date = db.Column(db.String) # TODO Modify for a date-specific format
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, nullable=False)
+    item_type = db.Column(db.String, nullable=False)
+    inventory_cost = db.Column(db.Float, nullable=False)
+    expiration_date = db.Column(db.DATE, nullable=False)
 
     def __init__(self, product_id, inventory_cost):
-        self.item_id = None # TODO Add logic to auto assign item_id
 # TODO Decide if product_id or item_type should be input
         self.product_id = product_id
         self.item_type = None # TODO Get item_type from Products table
         self.inventory_cost = inventory_cost
         self.expiration_date = None # TODO Set to today + shelf_life from Products table
 
-    def get_item(self, item_id)
+    def get_item(self, item_id):
         # TODO return all fields associated with item_id
         return []
 
     def __repr__(self):
-        return '{} {} {} {} {}'.format(self.item_id, self.product_id,self.item_type,
-                                          self.inventory_cost, self.expiration_date)
-"""
+        return '{} {} {} {} {}'.format(self.id, self.product_id,self.item_type,
+                                       self.inventory_cost, self.expiration_date)
 
 
 class Products(db.Model):
     __tablename__ = "products"
 
-    product_id = db.Column(db.Integer, primary_key=True)
-    supplier_id = db.Column(db.Integer)
-    inventory_count = db.Column(db.Integer)
-    min_inventory = db.Column(db.Integer)
-    shelf_life = db.Column(db.Integer)
-    standard_price = db.Column(db.Float)
-    sale_price = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True)
+    supplier_id = db.Column(db.Integer, nullable=False)
+    inventory_count = db.Column(db.Integer, nullable=False)
+    min_inventory = db.Column(db.Integer, nullable=False)
+    shelf_life = db.Column(db.Integer, nullable=False)
+    standard_price = db.Column(db.Float, nullable=False)
+    sale_price = db.Column(db.Float, nullable=False)
 
     def __init__(self, supplier_id, inventory_count, min_inventory, shelf_life, standard_price):
-        self.product_id = None  # TODO Add logic to auto assign product_id
         self.supplier_id = supplier_id
         self.inventory_count = inventory_count
         self.min_inventory = min_inventory
@@ -72,7 +70,7 @@ class Products(db.Model):
         return self.sale_price
 
     def __repr__(self):
-        return '{} {} {} {} {} {} {}'.format(self.product_id, self.supplier_id, self.inventory_count,
+        return '{} {} {} {} {} {} {}'.format(self.id, self.supplier_id, self.inventory_count,
                                              self.min_inventory, self.shelf_life, self.standard_price,
                                              self.sale_price)
 
@@ -80,65 +78,80 @@ class Products(db.Model):
 class Sales(db.Model):
     __tablename__ = "sales"
 
-    item_id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer)
-    item_type = db.Column(db.String)
-    price_sold = db.Column(db.Float)
-    inventory_cost = db.Column(db.Float)
-    transaction_id = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, nullable=False)
+    item_type = db.Column(db.String, nullable=False)
+    price_sold = db.Column(db.Float, nullable=False)
+    inventory_cost = db.Column(db.Float, nullable=False)
+    transaction_id = db.Column(db.Float, nullable=False)
 
     def __init__(self, item_id, price_sold, transaction_id):
-        self.item_id = item_id
-        self.product_id = None  # TODO Get product_id from Inventory table
         self.item_type = None  # TODO Get item_type from Inventory table
         self.price_sold = price_sold
         self.inventory_cost = None  # TODO Get inventory_cost from Inventory table
         self.transaction_id = transaction_id
 
     def __repr__(self):
-        return '{} {} {} {} {} {}'.format(self.item_id, self.product_id, self.item_type,
+        return '{} {} {} {} {} {}'.format(self.id, self.product_id, self.item_type,
                                           self.price_sold, self.inventory_cost, self.transaction_id)
 
 
 class Suppliers(db.Model):
     __tablename__ = "suppliers"
 
-    supplier_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
 
     def __init__(self, name, email):
-        self.supplier_id = None  # TODO Add logic to auto assign supplier_id
         self.name = name
         self.email = email
 
     def __repr__(self):
-        return '{} {} {}'.format(self.supplier_id, self.name, self.email)
+        return '{} {} {}'.format(self.id, self.name, self.email)
 
 
 class Transaction(db.Model):
     __tablename__ = "transactions"
 
-    cust_name = db.Column(db.String)
-    cust_contact = db.Column(db.String)
-    payment_type = db.Column(db.Integer)
-    date = db.Column(db.String)  # TODO Modify for a date-specific format
-    transaction_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    cust_name = db.Column(db.String, nullable=False)
+    cust_contact = db.Column(db.String, nullable=False)
+    payment_type = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DATE, nullable=False)
 
     def __init__(self, cust_name, cust_contact, payment_type):
         self.cust_name = cust_name
         self.cust_contact = cust_contact
         self.payment_type = payment_type
         self.date = None  # TODO Auto-assign date
-        self.transaction_id = None  # TODO Add logic to auto assign transaction_id
 
     def get_transaction(self, transaction_id):
         # TODO return all fields associated with transaction_id
         return []
 
     def __repr__(self):
-        return '{} {} {} {} {}'.format(self.cust_name, self.cust_contact, self.payment_type,
-                                       self.date, self.transaction_id)
+        return '{} {} {} {} {}'.format(self.id, self.cust_name, self.cust_contact, self.payment_type, self.date)
+
+
+class Discounts(db.Model):
+    __tablename__ = "discounts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, nullable=False)
+    start_date = db.Column(db.DATE, nullable=False)
+    end_date = db.Column(db.DATE, nullable=False)
+    discount = db.Column(db.Float, nullable=False) # TODO Is this a percentage or what?
+
+    def __init__(self, product_id, start_date, end_date, discount):
+        self.product_id = product_id
+        self.start_date = start_date
+        self.end_date = end_date
+        self.discount = discount
+
+    def __repr__(self):
+        return '{} {} {} {} {}'.format(self.id, self.product_id, self.start_date, self.end_date, self.discount)
+
 
 
 class User(db.Model):
