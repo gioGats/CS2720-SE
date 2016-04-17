50b732ded025a43f37e8e2c517455a95ac241ff7
gioGats <ryangiarusso@gmail.com>
2016-04-16 21:19:11 -0500

    POS_logic hell and confusion

--- Flask-App/POS_logic.py
+++ Flask-App/POS_logic.py
@@ -149,15 +149,30 @@ class TransactionRow(Row):
         self.payment_type = payment_type
         self.date = date
 
+
 class CashierRow(Row):
     def __init__(self, item_id, product_name, price):
+        """
+        Holds data for the display of an item in a customer's cart currently checking out.
+        :param item_id: int
+        :param product_name: str
+        :param price: float
+        :return: None
+        """
         Row.__init__(self)
         self.item_id = item_id
         self.product_name = product_name
         self.price = price
 
+
 class StockerRow(ItemRow):
     def __init__(self, product_id, inventory_cost):
+        """
+        Holds data for the display of an item currently being added to inventory.
+        :param product_id: int
+        :param inventory_cost: float
+        :return: None
+        """
         Row.__init__(self)
         self.product_id = product_id
         self.inventory_cost = inventory_cost
@@ -176,13 +191,29 @@ class Table:
 
 class CashierTable(Table):
 
-    def add_row(self, item_id, product_name, price): # Depend on what type of table
+    def add_row(self, item_id, product_name, price):
+        """
+        Adds a cashier row to the cashier table.
+        :param item_id: int
+        :param product_name: str
+        :param price: float
+        :return: None
+        """
         new_row = CashierRow(item_id, product_name, price)
         self.rowsList.append(new_row)
         self.rowCount += 1
         self.mostRecentRow = new_row
 
-    def edit_row(self, row_number, item_id, product_name, price): # Depend on what type of table
+    def edit_row(self, row_number, item_id, product_name, price):
+        """
+        Changes a current row at index row_number-1 to the parameters specified.
+        If any row parameter is the empty string, it will default to its current setting.
+        :param row_number: int
+        :param item_id: int
+        :param product_name: str
+        :param price: float
+        :return:
+        """
         if item_id == '':
             item_id = self.rowsList[row_number-1].item_id
         if product_name == '':
@@ -201,16 +232,42 @@ class CashierTable(Table):
         self.mostRecentRow = None
 
     def delete_row(self, row_number):
+        """
+        Deletes the row at index row_number-1
+        :param row_number: int
+        :return: None
+        """
         self.rowsList.pop(row_number-1)
 
 
 class StockerTable(Table):
 
     def add_row(self, product_id, inventory_cost):
-        pass
+        """
+        Adds a stocker row to the stocker table.
+        :param product_id: int
+        :param inventory_cost: float
+        :return: None
+        """
+        new_row = StockerRow(product_id, inventory_cost)
+        self.rowsList.append(new_row)
+        self.rowCount += 1
+        self.mostRecentRow = new_row
 
     def edit_row(self, row_number, product_id, inventory_cost):
-        pass
+        """
+        Changes a current row at index row_number-1 to the parameters specified.
+        If any row parameter is the empty string, it will default to its current setting.
+        :param row_number: int
+        :param product_id: int
+        :param inventory_cost: float
+        :return: None
+        """
+        if product_id == '':
+            product_id = self.rowsList[row_number-1].item_id
+        if inventory_cost == '':
+            inventory_cost = self.rowsList[row_number-1].product_name
+        self.rowsList[row_number-1] = StockerRow(product_id, inventory_cost)
 
     def clear_table(self):
         """
@@ -222,6 +279,11 @@ class StockerTable(Table):
         self.mostRecentRow = None
 
     def delete_row(self, row_number):
+        """
+        Deletes the row at index row_number-1
+        :param row_number: int
+        :return: None
+        """
         self.rowsList.pop(row_number-1)
 
 
