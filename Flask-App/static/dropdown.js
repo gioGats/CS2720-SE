81f67e3267cfa94f5a40dbafe2ad54dea8cee168
Death <bytepanther@gmail.com>
2016-04-16 19:11:42 -0500

    Added files via upload

---
+++ Flask-App/static/dropdown.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Jacob on 4/16/2016.
+ */
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
